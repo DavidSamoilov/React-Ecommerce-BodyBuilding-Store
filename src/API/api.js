04350ede112a6e0e69cc284abf1fa28598305071
David <dada7117@gmail.com>
2021-06-22 20:57:10 +0300

    dal get product details func added

--- src/API/api.js
+++ src/API/api.js
@@ -9,21 +9,34 @@ const api = (() => {
     }
     getCategories().then(result => console.log(result))
 
-    const getUserCartItems = async (userID)=> {
+    const getProductDetailsForDisplay = () => {
+        let productDetails = Promise.resolve([{name:"Protein Powder", small_desc:"Vanilla tasting protein bar 2.5 kg",
+        big_desc:"lorem lorem lorem lorem lorem lorem lorem " ,image :"png",categories:"Protein Food_Supplements"},
+        {name:"Shirt", small_desc:"Workout Shirt",
+        big_desc:"lorem lorem lorem lorem lorem lorem lorem " ,image :"png",categories:"Shirts Clothing"}
+
+    ])
+
+    // DataBase will fetch different items depending on what filters it will receive
+    return productDetails
+    }
+    getProductDetailsForDisplay().then(result => console.log(result))
+
+    const getUserCartProducts = async (userID)=> {
         // This select will take the userID and find all items in cart table that have the same id 
         // then it Will return an array of objects with {product_id:***, quantity:*}
 
         // UI will have will have a func to display cart items
-        let userCartItemsAndQuantity= Promise.resolve([{product_id:1,quantity:2},{product_id:2,quantity:1},{product_id:3,quantity:1}])
-        return userCartItemsAndQuantity
+        let userCartProductsAndQuantity= Promise.resolve([{product_id:1,quantity:2},{product_id:2,quantity:1},{product_id:3,quantity:1}])
+        return userCartProductsAndQuantity
 
     }
-    getUserCartItems().then(result => console.log(result))
+    getUserCartProducts().then(result => console.log(result))
 
 
     return {
         getCategories,
-        getUserCartItems
+        getUserCartProducts
     }
 })()
 
