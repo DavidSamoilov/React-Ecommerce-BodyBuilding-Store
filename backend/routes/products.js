d85c0cea19515eb69b77248e605883f1e5d70d0f
David <dada7117@gmail.com>
2021-07-07 10:55:22 +0300

    added get all products

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const connection = require("../MySQL/connection");
-const { Products } = require("../models/");
+const { Products, Images } = require("../models/");
 
 // Gets all Users
 router.get("/", async (req, res) => {
   try {
-    Products.findAll().then((allProducts) => res.json(allProducts));
+    Products.findAll({
+      include: ["images", "product_categories"],
+    }).then((users) => {
+      res.json(users);
+    });
+
+
+    // Products.findAll().then((allProducts) => res.json(allProducts));
   } catch (err) {
     res.send(err);
   }
