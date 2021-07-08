f46b96aeda12772391d1997dd6b592132108f272
David <dada7117@gmail.com>
2021-07-08 15:25:59 +0300

    remaking navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,33 +2,92 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
+import { NavDropdown } from "react-bootstrap";
+import { Form, Button, FormControl } from "react-bootstrap";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import { FaBars } from "react-icons/fa";
 
 const Navigation = () => {
   return (
-    <div>
-      {/* Nav */}
-      <Navbar fixed="top" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              src="https://www.crushpixel.com/big-static7/preview4/gym-weight-icon-140947.jpg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </Navbar.Brand>
-          <Nav className="justify-content-end">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Login/Register</Nav.Link>
-            <Nav.Link href="#Cart">
-              <i className="fas fa-shopping-cart"></i>
-            </Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </div>
+    <NavContainer>
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src="" alt="LOGO" srcset="" />
+          </Link>
+          <button type="button" className='nav-toggle'>
+            <FaBars />
+          </button>
+        </div>
+        
+      </div>
+    </NavContainer>
   );
 };
 
 export default Navigation;
+
+const NavContainer = styled.nav`
+  height: 5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .nav-center {
+    width: 90vw;
+    margin: 0 auto;
+    max-width: var(--max-width);
+  }
+
+  .nav-header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    img {
+      width: 175px;
+      margin-left: -15px;
+    }
+  }
+  .nav-toggle {
+    background: transparent;
+    border: transparent;
+    color: var(--clr-primary-5);
+    cursor: pointer;
+    svg {
+      font-size: 2rem;
+    }
+  }
+  .nav-links {
+    display: none;
+  }
+  .cart-btn-wrapper {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .nag-toggle {
+      display: none;
+    }
+    .nav-center {
+      display: grid;
+      grid-template-columns: auto 1fr auto;
+      align-items: center;
+    }
+    .nav-links {
+      display: flex;
+      justify-content: center;
+    }
+    li {
+      margin: 0 0.5rem;
+    }
+    a{
+      color :grey;
+      font-size:1rem;
+      text-transform: capitalize;
+      letter-spacing: var(--spacing);
+      padding :0.5rem;
+
+    }
+  }
+`;
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -17,27 +17,6 @@ const HomePage = () => {
 
   return (
     <div>
-      {/* Nav */}
-      <Navbar fixed="top" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              src="https://www.crushpixel.com/big-static7/preview4/gym-weight-icon-140947.jpg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </Navbar.Brand>
-          <Nav className="justify-content-end">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Login/Register</Nav.Link>
-            <Nav.Link href="#Cart">
-              <i className="fas fa-shopping-cart"></i>
-            </Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
       {/* Landing page hero */}
       <div className="hero">
         <div className="image-text">
