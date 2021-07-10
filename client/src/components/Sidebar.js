0ba95c7cfb243acd80e926ef128b856a142de6d4
David <dada7117@gmail.com>
2021-07-10 20:09:35 +0300

    side bar basics no styling, have to add logo

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { FaTimes } from "react-icons/fa";
+import styled from "styled-components";
 
 const Sidebar = () => {
+  const isOpen = true;
   return (
-    <div>
-      <h1>Sidebar</h1>
-    </div>
+    <SidebarContainer>
+      <aside
+        className={`${isOpen ? "sidebar show-sidebar" : "show-sidebar"}`}
+      ></aside>
+      <div className="sidebar-header">
+        <img src={logo} className="logo" alt="Davids gym"/>
+        <button className="close-btn" type="button">
+          <FaTimes/>
+        </button>
+      </div>
+    </SidebarContainer>
   );
 };
-
+const SidebarContainer = styled.div``;
 export default Sidebar;
