0cddaa8201e55a5c3ec435330dfdafa14b3de738
David <dada7117@gmail.com>
2021-07-10 19:09:06 +0300

    added cart and login button to navbar needs css*

--- client/src/components/CardButtons.js
+++ client/src/components/CardButtons.js
@@ -1,11 +1,29 @@
-import React from 'react'
-
+import React from "react";
+import { FaShoppingCart, FaUserPlus } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 const CardButtons = () => {
-    return (
-        <div>
-            Cart
-        </div>
-    )
-}
+  return (
+    <Wrapper className="cart-btn-wrapper">
+      <Link to="/cart" className="cart-btn">
+        Cart
+        <span className="cart-container">
+          <FaShoppingCart />
+          <span className="cart-value">12</span>
+        </span>
+      </Link>
+      <button type="button" className="auth-btn">
+          Login <FaUserPlus/>
+      </button>
+    </Wrapper>
+  );
+};
 
-export default CardButtons
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  width: 225px;
+`;
+
+export default CardButtons;
