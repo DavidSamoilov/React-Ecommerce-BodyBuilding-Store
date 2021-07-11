42de5312be64ef50f7c6714ec78e946a3a2fde61
David <dada7117@gmail.com>
2021-07-11 23:03:29 +0300

    add single product context

--- client/src/context/products_context.js
+++ client/src/context/products_context.js
@@ -19,10 +19,10 @@ const initialState = {
   products_error: false,
   products: [],
   featured_products: [],
+  single_product_loading: false,
+  single_product_error: false,
+  single_product: {},
 };
-// single_product_loading: false,
-// single_product_error: false,
-// single_product: {},
 
 const ProductsContext = React.createContext();
 
@@ -49,16 +49,16 @@ export const ProductsProvider = ({ children }) => {
   };
   
   
-  // const fetchSingleProduct = async (url) => {
-  //   dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
-  //   try {
-  //     const response = await axios.get(url);
-  //     const singleProduct = response.data;
-  //     dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct });
-  //   } catch (error) {
-  //     dispatch({ type: GET_SINGLE_PRODUCT_ERROR });
-  //   }
-  // };
+  const fetchSingleProduct = async (url) => {
+    dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const singleProduct = response.data;
+      dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct });
+    } catch (error) {
+      dispatch({ type: GET_SINGLE_PRODUCT_ERROR });
+    }
+  };
 
   useEffect(() => {
     fetchProducts(url);
@@ -67,7 +67,7 @@ export const ProductsProvider = ({ children }) => {
   return (
     <ProductsContext.Provider
       // , fetchSingleProduct
-      value={{ ...state, openSidebar, closeSidebar }}
+      value={{ ...state, openSidebar, closeSidebar,fetchSingleProduct }}
     >
       {children}
     </ProductsContext.Provider>
