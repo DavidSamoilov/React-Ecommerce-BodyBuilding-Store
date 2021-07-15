44e44ff68753ead3ebe4ac08d9a9b9e67167fc99
David <dada7117@gmail.com>
2021-07-15 19:37:11 +0300

    add context base

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,16 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 import { ProductsProvider } from "./context/products_context";
-import {FilterProvider} from "./context/filter_context"
+import { FilterProvider } from "./context/filter_context";
+import { CartProvider } from "./context/cart_context";
 ReactDOM.render(
   <React.StrictMode>
     <ProductsProvider>
-    <FilterProvider>
-      <App />
-    </FilterProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
     </ProductsProvider>
   </React.StrictMode>,
   document.getElementById("root")
---
+++ client/src/reducers/cart_reducer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const cart_reducer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default cart_reducer
