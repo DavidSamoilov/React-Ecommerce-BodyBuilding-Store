b7245d01d1e6c4a89ce464c0d4996aee0a76da8e
David <dada7117@gmail.com>
2021-07-15 21:39:27 +0300

    add add product to cart context

--- client/src/reducers/cart_reducer.js
+++ client/src/reducers/cart_reducer.js
@@ -10,21 +10,19 @@ import {
 const cart_reducer = (state, action) => {
   if (action.type === ADD_TO_CART) {
     const { id, amount, product } = action.payload;
-    const tempItem = state.cart.find((i) => i.id === id )
-    if (tempItem){
-
-
-    }else{
-        console.log(product);
-        const newItem = {
-            id,
-            name:product.name,
-            amount,
-            image:product.images[0].image,
-            price:product.price
-            // max: product.stock
-        }
-        return {...state , cart:[...state.cart,newItem]}
+    const tempItem = state.cart.find((i) => i.id === id);
+    if (tempItem) {
+    } else {
+      console.log(product);
+      const newItem = {
+        id,
+        name: product.name,
+        amount,
+        image: product.images[0].image,
+        price: product.price,
+        // max: product.stock
+      };
+      return { ...state, cart: [...state.cart, newItem] };
     }
   }
   return state;
