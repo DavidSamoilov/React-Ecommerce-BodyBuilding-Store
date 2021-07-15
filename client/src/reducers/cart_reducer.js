90dce7e656e80f680d2d0c624978f6f42665f93f
David <dada7117@gmail.com>
2021-07-15 23:01:06 +0300

    add the ability to check if item is already in cart and add to the amount of it

--- client/src/reducers/cart_reducer.js
+++ client/src/reducers/cart_reducer.js
@@ -12,6 +12,15 @@ const cart_reducer = (state, action) => {
     const { id, amount, product } = action.payload;
     const tempItem = state.cart.find((i) => i.id === id);
     if (tempItem) {
+      const tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === id) {
+          let newAmount = cartItem.amount + amount;
+          return { ...cartItem, amount: newAmount };
+        } else {
+          return cartItem;
+        }
+      });
+      return { ...state, cart: tempCart };
     } else {
       console.log(product);
       const newItem = {
@@ -20,6 +29,7 @@ const cart_reducer = (state, action) => {
         amount,
         image: product.images[0].image,
         price: product.price,
+        // this will be added to check max stock
         // max: product.stock
       };
       return { ...state, cart: [...state.cart, newItem] };
