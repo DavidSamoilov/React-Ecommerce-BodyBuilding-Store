fa98da0dceb4b22a297817c2b55f346af49610e6
David <dada7117@gmail.com>
2021-07-17 17:17:31 +0300

    add page hero styling

--- client/src/components/PageHero.js
+++ client/src/components/PageHero.js
@@ -1,20 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 const PageHero = ({ title, product }) => {
   return (
     <Wrapper>
-      <div className="section-center">
+      <div className='section-center'>
         <h3>
-          <Link to="/">Home</Link>/
-          {product && <Link to="/products">Products/</Link>}
-          {title}
+          <Link to='/'>Home</Link>
+          {product && <Link to='/products'>/ Products</Link>}/ {title}
         </h3>
       </div>
     </Wrapper>
-  );
-};
-const Wrapper = styled.section``;
+  )
+}
 
-export default PageHero;
+const Wrapper = styled.section`
+  background: black;
+  background-color: #000000;
+background-image: linear-gradient(315deg, #000000 0%, #b82e1f 74%);
+
+  width: 100%;
+  min-height: 10vh;
+  display: flex;
+  align-items: center;
+  /* color: var(--clr-primary-1); */
+  color: #532020;
+  a {
+    /* color: var(--clr-primary-3); */
+    color: black;
+    padding: 0.5rem;
+    transition: var(--transition);
+  }
+  a:hover {
+    color: var(--clr-primary-1);
+  }
+`
+
+export default PageHero
